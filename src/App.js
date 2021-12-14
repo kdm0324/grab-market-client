32328b666313f2a5734f57a123de031b6cbb5658
dongmin.kwak <dongmin.kwak@lgepartner.com>
2021-12-15 00:21:17 +0900

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MainPageComponent from "./main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <MainPageComponent />;
 }
 
-export default App;
+export default App; //다른  곳에서 사용 할 수 있도록 Export 되어있는 것임.
